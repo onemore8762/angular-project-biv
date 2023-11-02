8b6a9298c36a0106c95e0f11835fcce90093ec94
onemore8762 <denis.churkin.afikei@mail.ru>
2023-11-02 22:50:44 +0300

    feat: validate Form

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Store} from "@ngrx/store";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {addEmployee} from "../store/employees.actions";
@@ -17,21 +17,25 @@ interface IEmployeeForm {
 })
 export class EmployeeAddComponent {
     userForm = new FormGroup<IEmployeeForm>({
-        name: new FormControl('', {nonNullable: true}),
-        surname: new FormControl('', {nonNullable: true}),
-        patronymic: new FormControl('', {nonNullable: true}),
+        name: new FormControl('', {nonNullable: true, validators: Validators.required}),
+        surname: new FormControl('', {nonNullable: true, validators: Validators.required}),
+        patronymic: new FormControl('', {nonNullable: true, validators: Validators.required}),
     })
 
     constructor(private store: Store, private _snackBar: MatSnackBar) {
     }
 
     onSubmit() {
-        this.store.dispatch(addEmployee({...this.userForm.value, id: Math.random() * 10}))
-        this.userForm.reset()
-        this._snackBar.open(
-            'Сотрудник успешно добавлен',
-            'X',
-            {duration: 2000, horizontalPosition: "start"});
+        // Math.random() можно было использовать библу, но решил не тянуть лишнюю зависимость
+        // Понимаю что можно было использовать uuId
+      if (this.userForm.valid){
+          this.store.dispatch(addEmployee({...this.userForm.value, id: Math.random() * 10}))
+          this.userForm.reset()
+          this._snackBar.open(
+              'Сотрудник успешно добавлен',
+              'X',
+              {duration: 2000, horizontalPosition: "start"});
+      }
     }
 
 }
